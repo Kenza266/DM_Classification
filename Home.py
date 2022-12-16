920f155f20fb4c41dc7c81cdcb4a39af41931d12
Kenza266 <66864019+Kenza266@users.noreply.github.com>
2022-12-16 23:08:49 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.title('Dataset')
-data = pd.read_csv('C:\\Users\\DELL\\Downloads\\M2Code\\DM\\Project\\DM-main\\Send\\pages\\datset.csv') 
+data = pd.read_csv('datset.csv') 
 
 st.dataframe(data)
